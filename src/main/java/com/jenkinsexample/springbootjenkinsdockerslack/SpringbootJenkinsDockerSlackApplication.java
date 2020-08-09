3e246841b0e404545bb9c1a94a7f2844121429d4
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-08-09 20:02:21 +0900

    git init

---
+++ src/main/java/com/jenkinsexample/springbootjenkinsdockerslack/SpringbootJenkinsDockerSlackApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsexample.springbootjenkinsdockerslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerSlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDockerSlackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsexample/springbootjenkinsdockerslack/SpringbootJenkinsDockerSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsexample.springbootjenkinsdockerslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerSlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
